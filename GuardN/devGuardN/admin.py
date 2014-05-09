42977b5dbc919a6bff237389e1189b247dfb78f4
Judy Fong <judyyfong@gmail.com>
2014-05-09 11:55:08 -0700

    halfway through tutorial 2

--- GuardN/devGuardN/admin.py
+++ GuardN/devGuardN/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from devGuardN.models import Poll
+from devGuardN.models import Choice
 
 # Register your models here.
+#default way to register a model
+#admin.site.register(Poll)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+ 
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+    (None,               {'fields': ['question']}),
+    ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question','pub_date', 'was_published_recently')
+	  
+admin.site.register(Poll, PollAdmin)

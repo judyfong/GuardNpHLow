5e97879158b92da242d9af3197106831cab536d1
Judy Fong <judyyfong@gmail.com>
2014-05-09 11:16:05 -0700

    added polls and choices to app and db

--- GuardN/devGuardN/models.py
+++ GuardN/devGuardN/models.py
@@ -1,22 +1,18 @@
 from django.db import models
-
 import datetime
 from django.utils import timezone
 
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def __str__(self):
+        return self.question
 
-# Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date_published')
-	def __str__(self):
-		return self.question_text
-	def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)	
-	
-	
 class Choice(models.Model):
-    question = models.ForeignKey(Question)
+    poll = models.ForeignKey(Poll)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __str__(self):
-	    return self.choice_text
+        return self.choice_text
